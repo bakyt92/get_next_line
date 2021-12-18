c8efeb27836154c01f1e63915a8cf50e6054c55a
ufitzhug <ufitzhug@ge-f4.kzn.21-school.ru>
2021-12-18 18:12:31 +0300

    18122021_18-10

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ufitzhug <ufitzhug@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/18 18:02:22 by ufitzhug          #+#    #+#             */
+/*   Updated: 2021/12/18 18:02:52 by ufitzhug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 int main (void)
 {
 	int fd;
 
+
 	fd = open("text_test.txt", O_RDONLY);
 	if (fd < 0)
 		x == 0;
 	return (0);
+
+}
+
+char *get_next_line(int fd)
+{
 
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ufitzhug <ufitzhug@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/18 18:02:44 by ufitzhug          #+#    #+#             */
+/*   Updated: 2021/12/18 18:02:50 by ufitzhug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef INC_03_GET_NEXT_LINE_GET_NEXT_LINE_H
 # define INC_03_GET_NEXT_LINE_GET_NEXT_LINE_H
 # include <sys/stat.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
 #ifdefined BUFFER_SIZE
 #if BUFFER_SIZE < 0
